31c7a54fc44e830d9dc16b5e56aae87dfb33dea3
hey-vatsa <148935889+hey-vatsa@users.noreply.github.com>
2023-11-09 12:11:07 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
+  password:{ type: String, required: true },
   googleId: String, // Store Google ID for Google-authenticated users
   resetToken: String,
   resetTokenExpiration: Date,
