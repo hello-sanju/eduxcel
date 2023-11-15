75e17717268fef96095728e3ae51ec1a883da2c6
hey-vatsa <148935889+hey-vatsa@users.noreply.github.com>
2023-11-15 14:04:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -393,7 +393,9 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
-
+app.get('/', (req, res) => {
+  res.send('Welcome to My API');
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
